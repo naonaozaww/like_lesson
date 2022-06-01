ab2bb0e4deae7854eb99519fe71cb0be63c78ccd
runteq <support@runteq.jp>
2022-06-01 16:25:20 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+  resources :posts
+  resources :users, only: %i[new create]
+end
